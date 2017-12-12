6918e6b1569cb77df66cc1b426c8b860ff05b602
carlos-dendarys <carlos.dendarys@gicasolutions.com>
2017-12-12 18:25:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+//We setup the port of the application server.
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
