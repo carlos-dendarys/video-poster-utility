13dbe4be62f359d92e2793199739bf40b22c62bd
Carlos A. Dendarys <carlos@thirdlove.com>
2017-12-13 17:47:26 -0300

    I added GhostInspector tests.

---
+++ test-utils/run_ghostinspector_test.js
@@ -0,0 +1,7 @@
+var request = require('request');
+
+request('https://api.ghostinspector.com/v1/tests/5a319034e684912918944780/execute/?apiKey=d3d0cc19fe8d69244a284e7061523b89542ba5dc', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body) // Print the google web page.
+    }
+})
