5d51b779b7f3138d875d2bce6d1de66de9b9785c
carlos-dendarys <carlos.dendarys@gicasolutions.com>
2018-01-09 12:09:20 -0300

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ test('Test tools/delete_abandoned_carts', () => {
         let name = 'John';
         expect(name).toEqual('John');
 });
+//testq
 
 test('Test tools/delete_abandoned_carts', () => {
     let name = 'Carlos';
